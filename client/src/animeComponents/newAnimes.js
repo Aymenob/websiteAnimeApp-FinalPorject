d20e79f95e55eaec13b3acfb1108daccb582e254
Aymenob <aymenobba@gmail.com>
2022-09-28 18:50:34 +0100

    final project

--- client/src/animeComponents/newAnimes.js
+++ client/src/animeComponents/newAnimes.js
@@ -7,8 +7,8 @@ const NewAnimes = ({Rate,animePicture,animeName,Season}) => {
                 {Rate}
             </div>
             <div class="NewPicture">
-            <button style={{ marginLeft:"-0.1cm",marginTop:"0cm",position:"relative",minHeight:"2cm",maxHeight:'2cm',width:"2.2cm"}} onClick={()=>alert("its working")} type="button" >
-                <img  style={{marginLeft:"-0.2cm",marginTop:"-0.1cm", position:"relative",minHeight:"2cm",maxHeight:'2cm',width:"2.2cm"}} alt="" src={animePicture}></img>
+            <button style={{ marginLeft:"-0.1cm",marginTop:"0cm",height:"2cm",width:"2cm"}} onClick={()=>alert("its working")} type="button" >
+                <img  style={{marginLeft:"-1cm",marginTop:"-1cm", position:"absolute",minHeight:"2cm",maxHeight:'2cm',minWidth:"2.2cm",maxWidth:'2.2cm'}} alt="" src={animePicture}></img>
             </button>
             </div>
             <div class="NewInfo">
