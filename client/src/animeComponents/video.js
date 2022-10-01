bd597c66bc3e74d7c3cd456090571f9782716d5c
Aymenob <aymenobba@gmail.com>
2022-10-01 01:06:58 +0100

    final project

--- client/src/animeComponents/video.js
+++ client/src/animeComponents/video.js
@@ -5,7 +5,7 @@ const Video = ({url,Description}) => {
     
          <div class="episodeSection">
         <p class="Title">{Description}</p>
-      <iframe width="560" height="315" src={url.replace(/\"/g,'').replace(/\'/g,'')} title="" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;fullscreen" ></iframe>
+      <iframe width="560" height="315" src={url.replace(/\"/g,'').replace(/\'/g,'')} title="" frameborder="0"  allow="accelerometer;  clipboard-write; encrypted-media; gyroscope; picture-in-picture;fullscreen" ></iframe>
       
     </div>
     
