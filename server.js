f278494457b7ac1c37741bf144cca45f874e0261
Aymenob <aymenobba@gmail.com>
2022-10-12 12:41:04 +0100

    anime website

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express=require("express")
 const app=express()
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
+    next(); 
+})
 require("dotenv").config()
 app.listen(process.env.PORT ||7000,()=>{console.log("you server is running...")})
 
@@ -10,12 +16,7 @@ const path=require('path')
 
 const mongoose=require("mongoose") 
 mongoose.connect(process.env.URL,()=>{"your data base is connected"})
-app.use((req,res,next)=>{
-    res.setHeader('Access-Control-Allow-Origin','*');
-    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
-    res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
-    next(); 
-})
+
 app.use(express.json({limit:'50mb'}))
 app.use(fileUpload({useTempFiles : true}))
 app.use("/",router)
